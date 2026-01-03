acaf568ddf83e01e77ba7f44978b762955cbf772
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-01-03 20:41:54 +0000

    Delete boilerplate from React installation in Vite (logos, logo styling and all references to logos). Add type Post for API response to determine shape of data returned by the TSX. Type return for fetchPosts() and pass Post type as an array to the useQuery hook. Allow for TS to identify each prop including mapping through the title prop in the retrun. Avoids useQuery data prop typed as any.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from "react";
+import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+/* 
+Source of fake data for examples:
+"https://jsonplaceholder.typicode.com/posts"
+"https://jsonplaceholder.typicode.com/users"
+*/
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) throw new Error("Error fetching data");
+  return response.json();
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data } = useQuery<Post[]>({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+  if (!data) return <div>Loading…</div>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
