e36cd582b43a1d762ef2e46349a0ef44ffffcfbc
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-03 21:02:08 +0000

    Extract type for Post, fetchPosts(), useQuery hook and render into own component Posts. Move Posts to directory src7components. Add button with toggle in App.tsx to load the data, allows display of loading first fetch then instant cached data display subsequent fetches. Demonstrates autocaching which comes with useQuery hook

---
+++ src/components/Posts.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) throw new Error("Error fetching data");
+  return response.json();
+};
+
+export const Posts = () => {
+  const { data, isLoading, error } = useQuery<Post[]>({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error occured; {error.message}</p>;
+
+  return (
+    <>
+      {data?.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </>
+  );
+};
--- src/postTypes.ts
+++
@@ -1,6 +0,0 @@
-export interface postProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
