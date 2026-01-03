d100df3465ef75760f67d87b918874964b4a9310
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-01-03 21:02:08 +0000

    Extract type for Post, fetchPosts(), useQuery hook and render into own component Posts. Move Posts to directory src7components. Add button with toggle in App.tsx to load the data, allows display of loading first fetch then instant cached data display subsequent fetches. Demonstrates autocaching which comes with useQuery hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
-//import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
+import { Posts } from "./components/Posts";
+
 /* 
 Source of fake data for examples:
 "https://jsonplaceholder.typicode.com/posts"
 "https://jsonplaceholder.typicode.com/users"
 */
 
-type Post = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-};
-
-const fetchPosts = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  if (!response.ok) throw new Error("Error fetching data");
-  return response.json();
-};
-
 function App() {
-  const { data, isLoading, error } = useQuery<Post[]>({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error occured; {error.message}</p>;
-
+  const [isMounted, setIsMounted] = useState(false);
   return (
     <>
-      {data?.map((post) => (
-        <p key={post.id}>{post.title}</p>
-      ))}
+      <button onClick={() => setIsMounted((prev) => !prev)}>Toggle</button>
+      {isMounted && <Posts />}
     </>
   );
 }
