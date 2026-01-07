7d0fdfe120bc7dfca44e4071c8b2e886aff610d8
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-01-07 17:55:25 +0000

    Commit completed { mutate} object with 3 callbacks onSuccess, onMutate, onError. Add explanatory comments. NB: would usually use  declared parameters err and newPost in onError block, not done here as simple tutorial concerned with the React Query hooks operation not building actual app.

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -16,15 +16,41 @@ export const CreatePost: React.FC = () => {
 
   const queryClient = useQueryClient();
 
-  // Typical use cases: create / update / delete operations
-  // useMutate allows us to create changes in the state and keep the
-  // server state in sync with the UI
+  /* Typical use cases for useMutate gook: create / update / delete operations
+  useMutate allows us to create changes in the state and keep the server state 
+  in sync with the UI. Typically 3 callback situations: 
+  onSuccess
+  onMutate
+  onError
+  */
   const { mutate } = useMutation({
     mutationFn: createPost,
     onSuccess: () => {
       // if add new post, previously executed query is now
       // out of date -  invalidate it to cause a refetch
       queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+    onMutate: async (newPost) => {
+      await queryClient.cancelQueries({ queryKey: ["posts"] });
+      const previousPosts = queryClient.getQueryData<Post[]>(["posts"]);
+      queryClient.setQueryData<Post[]>(["posts"], (old) => {
+        // Guard to handle the default type of any for property old
+        if (!old)
+          // Cache was empty
+          return [{ id: Date.now(), ...newPost }];
+        // Cache existed
+        return [...old, { id: Date.now(), ...newPost }];
+      });
+
+      return { previousPosts };
+    },
+    /* State was set for previousPosts so can now simply revert to 
+    the state before the error happened without handling the mutation 
+    from the onMutate block */
+    onError: (err, newPost, context) => {
+      if (context?.previousPosts) {
+        queryClient.setQueryData<Post[]>(["posts"], context.previousPosts);
+      }
     },
   });
 
